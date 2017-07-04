65e5924c42d7f9991806b6b74dcc6f851e2af30f
agnescexariana <agnescexa@gmail.com>
2017-07-04 18:18:28 +0700

    Add files via upload

---
+++ subscriber.py
@@ -0,0 +1,136 @@
+#!flask-env/bin/python
+import paho.mqtt.client as mqtt
+import json, MySQLdb
+import thread, time
+
+
+def on_connect(client, userdata, flags, rc):
+ 	print("connect rc: "+str(rc))
+
+def on_subscribe(client, userdata, mid, granted_qos):
+	print("Subscribed: "+" "+str(granted_qos))
+
+def on_message(client, userdata, message):
+    # # PENGUJIAN
+    # arrived_time = time.time()
+    # data = json.loads(message.payload)
+    # delay = arrived_time - data['start_time']
+    # # print("Received message on topic : " + message.topic + " | publish with QoS : " + str(message.qos) + " Delay : " + str(delay))#utk pengujian variasi ukuran data
+    # print "Received message Delay : "+str(delay)#untuk pengujian standard
+    
+    # namadb="rumahsakit"+userdata
+    # d = MySQLdb.connect(host="127.0.0.1",
+    #                             user = "root",
+    #                             passwd = "",
+    #                             db = ""+str(namadb))
+    
+
+ ############################################################
+    print(">> Received message : " + str(message.payload))
+    print(">> On topic : " + message.topic + " with QoS : " + str(message.qos))
+    data = json.loads(message.payload) #parse json
+    print ">> Action : " + data['status']
+    
+    curs=d.cursor()
+
+	# #INSERT DATA
+    if data['status'] == "insert":
+    	curs.execute("INSERT into pasien VALUES %s", (data['datapasien'],))
+    	d.commit()
+    	curs.close()
+
+    #DELETE DATA	
+    if data['status'] == "delete":
+    	id_pasienpost = data['datapasien']
+    	curs.execute("DELETE FROM pasien WHERE id_pasien=%s",(id_pasienpost,))
+    	d.commit() 
+    	curs.close()
+
+    # UPDATE DATA
+    if data['status'] == "update":
+    	row = data['datapasien']
+        i = len(row)
+        print i
+        for i in row :
+                id_pasien = row [0]
+                nama = row[1]
+                gender = row[2]
+                agama = row[3]
+                tgl = row[4]
+                bulan = row[5]
+                tahun = row[6]
+                email = row[7]
+                tlp = row[8]
+                alamat = row[9]
+                darah = row[10]
+                riwayat = row[11]
+        
+        # curs = d.cursor()
+        curs.execute("""UPDATE pasien SET nama=%s, 
+                                            gender=%s, 
+                                            agama=%s,
+                                            BirthDate=%s, 
+                                            BirthMonth=%s, 
+                                            BirthYear=%s, 
+                                            Email_id=%s, 
+                                            tlp=%s, 
+                                            alamat=%s, 
+                                            darah=%s,
+                                            riwayat_penyakit=%s 
+                                            WHERE id_pasien=%s""", 
+                        (nama, gender, agama, tgl, bulan, tahun, email, tlp, alamat, darah, riwayat, id_pasien,))
+        d.commit()
+        curs.close()
+
+
+    print (">> Data Successfully Subscribed")
+
+
+def on_log(client, userd, level, string):
+	print(string)
+
+def h():
+    global input_client
+    print "Client ID : " +str(input_client)
+    client = mqtt.Client("subs"+str(input_client),clean_session=False)
+    client.on_connect = on_connect
+    client.connect("127.0.0.1", 1883)
+    client.subscribe("pubsub/pasien/#", qos=1)
+    client.on_subscribe = on_subscribe
+    client.on_message = on_message
+    client.loop_forever()
+
+	#Untuk Pengujian
+def new_threads(id):
+    client = mqtt.Client("subb"+str(id), userdata=str(id), clean_session=True)
+    client.on_connect = on_connect
+    client.connect("127.0.0.1", 1883)
+    client.subscribe("pubsub/#", qos=1)
+    client.on_subscribe = on_subscribe
+    client.on_message = on_message
+    client.loop_forever()
+
+try:
+    input_client = 0
+    input_client = input("Input ID Client : ")
+    namadb="rumahsakit"+str(input_client)
+    d = MySQLdb.connect(host="127.0.0.1",
+                                user = "root",
+                                passwd = "",
+                                db = ""+str(namadb))
+    h()
+
+except KeyboardInterrupt:
+    print ("::Connection Closed") 
+
+#     # START_THREAD
+#     input_client = input("Input Subscriber : ")
+#     for i in range (input_client):
+#         # string="string"+str(i)
+#         thread.start_new_thread( new_threads, (i,))
+#         time.sleep(0.1)     
+# except:
+# 	print "Error: unable to start thread"
+
+# while 1:
+# 	pass
---
+++ ws-flask.py
@@ -0,0 +1,184 @@
+#!flask-env/bin/python
+import MySQLdb		
+from flask import Flask, request
+import paho.mqtt.client as mqtt
+import json
+
+app = Flask(__name__)
+
+db = MySQLdb.connect(host="127.0.0.1",
+                           user = "root",
+                           passwd = "",
+                           db = "rumahsakit")
+
+# data_pasien2 = [{"id_pasien" : "5",
+# 	"nama": "agnesss",
+# 	"gender" : "Perempuan",
+# 	"agama" : "Kristen",
+# 	"tgl" : "26",
+# 	"bulan" : "Maret",
+# 	"tahun" : "1994",
+# 	"email" : "agnes@gmail.com",
+# 	"tlp" : "082125356750",
+# 	"alamat_post" : "Jalan setia",
+# 	"darah" : "B",
+# 	"riwayat" : "Asma"},
+	
+# 	{"id_pasien" : "2",
+# 	"nama": "agus",
+# 	"gender" : "Laki-laki",
+# 	"agama" : "Islam",
+# 	"tgl" : "20",
+# 	"bulan" : "Maret",
+# 	"tahun" : "1989",
+# 	"email" : "agus@gmail.com",
+# 	"tlp" : "08131356750",
+# 	"alamat_post" : "Jalan bendungan",
+# 	"darah" : "A",
+# 	"riwayat" : "Alergi"}]
+   
+
+@app.route('/', methods=['GET'])
+def index():
+    cur = db.cursor()
+    cur.execute('''SELECT * FROM pasien''')
+    all_rows = cur.fetchall()
+    data = json.dumps(all_rows)
+    print data
+    cur.close()
+    return data
+
+@app.route('/print_data2', methods=['GET'])
+def get_pasien():
+	cur = db.cursor()
+	id_pasienpost = request.args.get('id_pasien')
+	cur.execute('''SELECT * FROM pasien WHERE id_pasien=%s ''',(id_pasienpost,))
+	data_pasien = cur.fetchone()
+	data = json.dumps(data_pasien) #encode
+	print "Get data =" + data	
+	cur.close()
+	return data
+
+@app.route('/registrasi', methods = ['GET'])
+def registrasi():
+	id_pasienpost = request.args.get('id_pasien')
+	nama_post = request.args.get('nama')
+	gender_post = request.args.get('gender')
+	agama_post = request.args.get('agama')
+	tgl_post = request.args.get('BirthDate')
+	bulan_post = request.args.get('BirthMonth')
+	tahun_post = request.args.get('BirthYear')
+	email_post = request.args.get('Email_id')
+	tlp_post = request.args.get('tlp')
+	alamat_post = request.args.get('alamat')
+	darah_post = request.args.get('darah')
+	riwayat_post = request.args.get('riwayat_penyakit')
+
+	cur = db.cursor()
+	cur.execute("""INSERT INTO pasien (id_pasien,
+					nama,
+					gender,
+					agama, 
+					BirthDate,
+ 					BirthMonth,
+					BirthYear,
+					Email_id, 
+					tlp, 
+					alamat,
+					darah,
+					riwayat_penyakit)
+			values(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)""", (id_pasienpost, nama_post, gender_post, agama_post, tgl_post, bulan_post, tahun_post, email_post, tlp_post, alamat_post, darah_post, riwayat_post))
+	db.commit() #to tell the database to save all the changes
+	
+	cur.execute('''SELECT * FROM pasien WHERE id_pasien=%s ''',(id_pasienpost,))
+	all_rows = cur.fetchone()
+	data = {"status": "insert", "datapasien": all_rows}
+	client.publish("pubsub/pasien/2", payload=json.dumps(data), qos=1) #publish data into broker
+	
+	print ">> Data Successfully Inserted"
+	cur.close()
+	return "Succesfully inserted into database"
+
+
+@app.route('/update', methods=['PUT'])
+def update_pasien():
+		cur = db.cursor()
+		id_pasienpost = request.args.get('id_pasien')
+		nama_post = request.args.get('nama')
+		gender_post = request.args.get('gender')
+		agama_post = request.args.get('agama')
+		tgl_post = request.args.get('BirthDate')
+		bulan_post = request.args.get('BirthMonth')
+		tahun_post = request.args.get('BirthYear')
+		email_post = request.args.get('Email_id')
+		tlp_post = request.args.get('tlp')
+		alamat_post = request.args.get('alamat')
+		darah_post = request.args.get('darah')
+		riwayat_post = request.args.get('riwayat_penyakit')
+
+		if nama_post:
+			cur.execute("UPDATE pasien SET nama=%s WHERE id_pasien=%s", (nama_post, id_pasienpost,))
+			db.commit()
+		if gender_post:
+			cur.execute("UPDATE pasien SET gender=%s WHERE id_pasien=%s", (gender_post, id_pasienpost,))
+			db.commit()
+		if agama_post:
+			cur.execute("UPDATE pasien SET agama=%s WHERE id_pasien=%s", (agama_post, id_pasienpost,))
+			db.commit()
+		if tgl_post:
+			cur.execute("UPDATE pasien SET BirthDate=%s WHERE id_pasien=%s", (tgl_post, id_pasienpost,))
+			db.commit()
+		if bulan_post:
+			cur.execute("UPDATE pasien SET BirthMonth=%s WHERE id_pasien=%s", (bulan_post, id_pasienpost,))
+			db.commit()
+		if tahun_post:
+			cur.execute("UPDATE pasien SET BirthYear=%s WHERE id_pasien=%s", (nama_post, id_pasienpost,))
+			db.commit()
+		if email_post:
+			cur.execute("UPDATE pasien SET Email_id=%s WHERE id_pasien=%s", (email_post, id_pasienpost,))
+			db.commit()
+		if tlp_post:
+			cur.execute("UPDATE pasien SET tlp=%s WHERE id_pasien=%s", (tlp_post, id_pasienpost,))
+			db.commit()
+		if alamat_post:
+			cur.execute("UPDATE pasien SET alamat=%s WHERE id_pasien=%s", (alamat_post, id_pasienpost,))
+			db.commit()
+		if darah_post:
+			cur.execute("UPDATE pasien SET darah=%s WHERE id_pasien=%s", (darah_post, id_pasienpost,))
+			db.commit()
+		if riwayat_post:
+			cur.execute("UPDATE pasien SET riwayat_penyakit=%s WHERE id_pasien=%s", (riwayat_post, id_pasienpost,))
+			db.commit()
+
+		cur.execute('''SELECT * FROM pasien WHERE id_pasien=%s ''',(id_pasienpost,))
+		datapasien = cur.fetchone()
+		data = {"status": "update", "datapasien": datapasien}
+		client.publish("pubsub/pasien/2", payload=json.dumps(data), qos=1)
+		
+		print ">> Data Successfully Updated"
+		cur.close()
+		return "Data Successfully Updated"
+
+		
+
+@app.route('/delete', methods=['DELETE'])
+def delete_pasien():
+	cur = db.cursor()
+	id_pasienpost = request.args.get('id_pasien')
+	cur.execute("DELETE FROM pasien WHERE id_pasien=%s",(id_pasienpost,))
+	db.commit() # Commit the change
+	data = {"status": "delete", "datapasien": id_pasienpost}
+	client.publish("pubsub/pasien/2", payload=json.dumps(data), qos=1)
+	
+	print ">> Data Successfully Deleted"
+	cur.close
+	return "Data Successfully Deleted"	
+
+
+
+if __name__ == "__main__":
+    client = mqtt.Client("pub1", clean_session=False)
+    client.connect("127.0.0.1", 1883)
+    client.publish('debug', 'server running')
+    app.debug = True
+    app.run()
